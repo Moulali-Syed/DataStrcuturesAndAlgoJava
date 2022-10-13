ef2dbcd4f827bbc8caa4668515c37d4e95839738
Moulali Syed <syedmoulali1997@gmail.com>
2022-10-13 10:38:39 +0530

    sorting

---
+++ DSA/src/jSearchingSorting/GRotateArray.java
@@ -0,0 +1,84 @@
+package jSearchingSorting;
+/*
+Rotate array
+
+You have been given a random integer array/list(ARR) of size N. Write a function that rotates the given array/list by D elements(towards the left).
+ Note:
+Change in the input array/list itself. You don't need to return or print the elements.
+Input format :
+The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+First line of each test case or query contains an integer 'N' representing the size of the array/list.
+
+Second line contains 'N' single space separated integers representing the elements in the array/list.
+
+Third line contains the value of 'D' by which the array/list needs to be rotated.
+Output Format :
+For each test case, print the rotated array/list in a row separated by a single space.
+
+Output for every test case will be printed in a separate line.
+Constraints :
+1 <= t <= 10^4
+0 <= N <= 10^6
+0 <= D <= N
+Time Limit: 1 sec
+Sample Input 1:
+1
+7
+1 2 3 4 5 6 7
+2
+Sample Output 1:
+3 4 5 6 7 1 2
+Sample Input 2:
+2
+7
+1 2 3 4 5 6 7
+0
+4
+1 2 3 4
+2
+Sample Output 2:
+1 2 3 4 5 6 7
+3 4 1 2
+ */
+public class GRotateArray {
+
+	public static void rotateMethod1(int[] arr,int d) {
+		
+		for(int i=1;i<=d;i++) {
+			int temp = arr[0];
+			for(int j=1;j<arr.length;j++) {
+				arr[j-1] = arr[j];
+			}
+			arr[arr.length-1] = temp;
+			
+		}
+	}
+	public static void rotateMethod2(int[] arr,int d) {
+		int[] ar = new int[d]; 
+		for(int i=0;i<d;i++) {
+			ar[i] = arr[i];
+		}
+		
+		for(int i=0;i<arr.length-d;i++) {
+			arr[i] = arr[i+d];
+		}
+		for(int i=ar.length-1;i>=0;i--) {
+			arr[arr.length-1-i] = ar[ar.length-1-i];
+		}
+	}
+	//method 3 - reverse all elements , then reverse n-d and d elements
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,4,5,6};
+		/*
+		rotateMethod1(arr,3);
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		*/
+		rotateMethod2(arr,3);
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+}
